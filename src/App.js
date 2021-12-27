b325a4b4985af72a9b45d42c0caa9c528f565775
daokhoi2k2 <ktbmariokiller@gmail.com>
2021-12-27 18:06:09 +0700

    Bài 8

--- src/App.js
+++ src/App.js
@@ -7,18 +7,39 @@ function App() {
 
   const increaseNumber = () => {
     setNumber((prev) => ++prev);
-  }
+  };
+
+  const handleClickMe = () => {
+    console.log("Click me nè")
+  };
+
   return (
     <>
       <Header />
-      <div>
-        Number: {number}
+      <div>Number: {number}</div>
+      <div className='d-flex'>
+        <Product
+          name='CSGO'
+          setNumber={setNumber}
+          price='360.000đ'
+          image='https://picsum.photos/600/300'
+        >
+          ASD
+        </Product>
+        <Product
+          name='PUBG'
+          setNumber={setNumber}
+          price='320.000đ'
+          image='https://picsum.photos/600/300'
+        />
+        <Product
+          name='Sea Of Thieves'
+          setNumber={setNumber}
+          price='250.000đ'
+          image='https://picsum.photos/600/300'
+        />
       </div>
-      <div className="d-flex">
-          <Product name="CSGO" setNumber={setNumber} price="360.000đ" image="https://picsum.photos/600/300">ASD</Product>
-          <Product name="PUBG" setNumber={setNumber} price="320.000đ" image="https://picsum.photos/600/300" />
-          <Product name="Sea Of Thieves" setNumber={setNumber} price="250.000đ" image="https://picsum.photos/600/300"  />
-      </div>
+      <button className="btn btn-success" onClick={handleClickMe}>Click me!</button>
     </>
   );
 }
--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,61 +1,86 @@
 import React from "react";
 import { Component } from "react/cjs/react.production.min";
 
-const Product = ({
-  image,
-  name,
-  price,
-  setNumber
-}) => {
-  const increaseNumber = () => {
-    setNumber((prev) => ++prev);
+// const Product = ({
+//   image,
+//   name,
+//   price,
+//   setNumber
+// }) => {
+//   const increaseNumber = () => {
+//     setNumber((prev) => ++prev);
+//   }
+
+//   const handleClick = () => {
+//     console.log("Chạy ở lần render đầu");
+//   }
+
+//   return (
+//     <div className='card m-2' style={{ width: "18rem" }}>
+//     <img
+//       src={image}
+//       className='card-img-top'
+//       alt='...'
+//     />
+//     <div className='card-body'>
+//       <h5 className='card-title'>{name}</h5>
+//       <p className='card-text'>
+//         {price}
+//       </p>
+//       <button onClick={handleClick} href='#' className='btn btn-primary'>
+//         Buy
+//       </button>
+//       {/* <a onClick={increaseNumber} href='#' className='btn btn-primary'>
+//         Buy
+//       </a> */}
+//     </div>
+//   </div>
+//   );
+// };
+
+class Product extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log(this)
+    this.onHandleClick = this.handleClick.bind(this.props.name);
   }
 
-  return (
-    <div className='card m-2' style={{ width: "18rem" }}>
-    <img
-      src={image}
-      className='card-img-top'
-      alt='...'
-    />
-    <div className='card-body'>
-      <h5 className='card-title'>{name}</h5>
-      <p className='card-text'>
-        {price}
-      </p>
-      <a onClick={increaseNumber} href='#' className='btn btn-primary'>
-        Buy
-      </a>
-    </div>
-  </div>
-  );
-};
+  // handleClick = () => {
+  //   // this.props.setNumber((prev) => prev + 1);
+  //   console.log(this.props);
+  //   // alert(this.props.name)
+  //   // alert()
+  // };
 
-// class Product extends Component {
-//     member() {
-//         console.log("Hello");
-//     }
-//     render() {
-//       console.log(this.props)
-//     return (
-//       <div className='card m-2' style={{ width: "18rem" }}>
-//         <img
-//           src={this.props.image}
-//           className='card-img-top'
-//           alt='...'
-//         />
-//         <div className='card-body'>
-//           <h5 className='card-title'>{this.props.name}</h5>
-//           <p className='card-text'>
-//             {this.props.price}
-//           </p>
-//           <a onClick={} href='#' className='btn btn-primary'>
-//             Buy
-//           </a>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+  // handleClick() {
+  //   console.log(this)
+  // }
+
+  // Nếu sử dụng function bình thường thì sẽ phải bind this
+  handleClick() {
+    console.log("This", this);
+  }
+
+  // Sử dụng arrow function để không nhận hàm là 1 context
+  // handleClick = () => {
+  //   console.log(this);
+  // }
+
+  render() {
+    return (
+      <div className='card m-2' style={{ width: "18rem" }}>
+        <img src={this.props.image} className='card-img-top' alt='...' />
+        <div className='card-body'>
+          <h5 className='card-title'>{this.props.name}</h5>
+          <p className='card-text'>{this.props.price}</p>
+          <button onClick={ this.onHandleClick } className='btn btn-primary'>
+            Buy
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Product;
